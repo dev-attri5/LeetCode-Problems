eca26d1c32c9fc511f17010f27b543c8fc458447
Dev <devattri410@gmail.com>
2026-08-13 21:38:37 +0530

    Time: 1 ms (98.21%), Space: 42.9 MB (80.51%) - LeetHub

---
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String digitSum(String s, int k) {
+        while (s.length() > k) {
+            StringBuilder newStr = new StringBuilder();
+            
+            for (int i = 0; i < s.length(); i += k) {
+                int sum = 0;
+                
+                for (int j = i; j < s.length() && j < i + k; j++) {
+                    sum += s.charAt(j) - '0'; 
+                }
+                
+                newStr.append(sum);
+            }
+            
+            s = newStr.toString();
+        }
+        
+        return s;
+    }
+}
