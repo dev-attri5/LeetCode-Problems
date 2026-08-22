7ab5be6c49b98155cf3b01a00a8776e98dfd55d1
Dev <devattri410@gmail.com>
2026-08-22 13:04:35 +0530

    Time: 0 ms (100%), Space: 46.5 MB (63.18%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,66 @@
+class Solution {
+
+    int search(MountainArray mountainArr, int target){
+     int peak = peakIndexInMountainArray(mountainArr);
+  
+     
+     int firstTry = orderAgnosticBS(mountainArr, target, 0 ,peak);
+     if(firstTry != -1){
+       return firstTry;
+     }
+        return orderAgnosticBS(mountainArr, target, peak+1, mountainArr.length()-1);
+    }
+
+    int peakIndexInMountainArray(MountainArray mountainArr){
+        int start = 0;
+        int end = mountainArr.length()-1;
+
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(mountainArr.get(mid) > mountainArr.get(mid+1)){
+
+              end = mid;
+
+            }else {
+                start = mid +1;
+            }
+        }
+        return start;
+    }
+    
+
+    int orderAgnosticBS(MountainArray mountainArr, int target, int start, int end) {
+    
+        boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
+
+        while(start <= end) {
+           
+            int mid = start + (end - start) / 2;
+
+            if (mountainArr.get(mid) == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < mountainArr.get(mid)) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > mountainArr.get(mid)) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int findInMountainArray(int target, MountainArray mountainArr){
+        return search(mountainArr, target);
+    }
+    }
+    
+    
