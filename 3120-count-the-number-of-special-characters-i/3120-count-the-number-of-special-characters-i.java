7d051023f75035db5727f81eab0a69611e15bdec
Dev <devattri410@gmail.com>
2026-08-29 21:00:25 +0530

    Time: 1 ms (100%), Space: 43.6 MB (79.61%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        boolean[] seen = new boolean[128];
+
+        for(int i = 0; i < word.length(); i++){
+            seen[word.charAt(i)] = true;
+        }
+        int count = 0; 
+
+        for(int i = 0; i< 26; i++){
+            if(seen['a' + i] && seen['A' + i]){
+            count++;
+            }
+        }
+        return count;
+    }
+}
